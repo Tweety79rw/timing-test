6eea28d0cbf6fd80def3e6deafc5590ada291b6f
Tweety79rw <ryan.wilson2@email.wsu.edu>
2019-07-29 22:54:19 -0700

    added cellsize to simulate chars on a console

--- faller.js
+++ faller.js
@@ -1,8 +1,12 @@
 class Faller {
-  constructor(x, y) {
+  constructor(x, y, cellSize) {
     this.pos = createVector(x, y);
     this.vel = createVector(random(-2, 2), random(-2, 2));
+    this.vel.div(cellSize);
     this.acc = createVector();
+    this.cellSize = cellSize;
+    this.strokeSize = 15;
+    this.halfStroke = this.strokeSize / 2;
   }
   update() {
     this.vel.add(this.acc);
@@ -10,10 +14,18 @@ class Faller {
     this.acc.mult(0);
   }
   bounce() {
-    if(this.pos.y <= 0 || this.pos.y >= height) {
-      this.vel.y *= -1;
+    if(this.pos.y * this.cellSize < this.halfStroke) {
+      this.pos.y = this.halfStroke / this.cellSize;
+      this.vel.y *= -1
+    } else if(this.pos.y * this.cellSize > height - this.halfStroke){
+      this.pos.y = (height - this.halfStroke) / this.cellSize;
+      this.vel.y *= -1
     }
-    if(this.pos.x < 0 || this.pos.x > width) {
+    if(this.pos.x* this.cellSize < this.halfStroke) {
+      this.pos.x = this.halfStroke / this.cellSize;
+      this.vel.x *= -1;
+    } else if(this.pos.x * this.cellSize > width - this.halfStroke) {
+      this.pos.x = (width - this.halfStroke) / this.cellSize;
       this.vel.x *= -1;
     }
   }
@@ -22,9 +34,9 @@ class Faller {
   }
   render() {
     push();
-    translate(this.pos.x, this.pos.y);
+    translate(Math.round(this.pos.x) * this.cellSize, Math.round(this.pos.y) * this.cellSize);
     stroke(255);
-    strokeWeight(5);
+    strokeWeight(this.strokeSize);
     point(0, 0);
     pop();
   }
--- sketch.js
+++ sketch.js
@@ -1,15 +1,18 @@
 let faller;
 let timer;
+const cellSize = 50;
 function setup() {
-  createCanvas(1600,800);
-  faller = new Faller(width/2, 0);
+  createCanvas(800, 600);
+  faller = new Faller(width/2/cellSize, 0, cellSize);
   timer = new Timer();
 }
 function draw() {
   background(0);
   timer.step();
   let gravity = createVector(0, 9.8);
+  gravity.div(cellSize);
   gravity.mult(timer.delta);
+
 
   faller.addForce(gravity);
   faller.update();
